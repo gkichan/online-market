a524512bc097329b73b0cf34b5a76bb17df33f7f
ArgeWorks <argeworks@icloud.com>
2018-04-12 09:43:26 +0300

    Organizing order and accuracy in the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,36 @@
-const gulp = require('gulp'); // init Gulp
-const autoprefixer = require('gulp-autoprefixer'); // init Gulp-autoprefixer
-const concat = require('gulp-concat'); // Init Gulp concat
+//Init Gulp
+const gulp = require('gulp');
+//Init Gulp-Autoprefixer
+const autoprefixer = require('gulp-autoprefixer');
+//Init Gulp-Concat
+const concat = require('gulp-concat');
 
-// Autoprefixer
-gulp.task('autoprefixer',function () {
-  gulp.src('source/css/**/*.css')
-      .pipe(autoprefixer({
-        browsers: ['last 2 versions']
-      }))
-      .pipe(gulp.dest('app/css'))
+//Autoprefixer Settings
+gulp.task('autoprefixer', function() {
+    gulp.src('source/css/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 version']
+        }))
+        .pipe(gulp.dest('app/css/'))
 });
 
-// Concat all css files
-gulp.task('concat-css', function () {
-  gulp.src([
-      'app/css/fonts.css',
-      'app/css/reset.css',
-      'app/css/components/*.css',
-      'app/css/pages/*.css'
-  ])
-      .pipe(concat('style.css'))
-      .pipe(gulp.dest('app/css'))
+//Concat Settings
+gulp.task('concat-css', function() {
+    gulp.src([
+            'app/css/fonts.css', 
+            'app/css/reset.css', 
+            'app/css/components/*.css', 
+            'app/css/pages/*.css'
+        ])
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('app/css/'))
 });
 
-// Watch
-gulp.task('watch',['autoprefixer','concat-css'],function () {
-  gulp.watch('source/css/**/*.css',['autoprefixer']); // watch style directory and run task 'autoprefixer'
-  gulp.watch('app/css/**/*.css', ['concat-css']); // watch and run concat-css
+//Create watch task
+gulp.task('watch', ['autoprefixer', 'concat-css'], function() {
+    gulp.watch('source/css/**/*.css', ['autoprefixer']); //Отслеживаем изменения в исходных стилях и выполняем autoprefixer
+    gulp.watch('app/css/**/*.css', ['concat-css']); //Отслеживаем изменения в готовых стилях и выполняем concat
 });
 
-// Default task
-gulp.task('default',['watch']);
+//Set watch task as default task
+gulp.task('default', ['watch']);
