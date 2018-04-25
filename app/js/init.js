39807b95875aa2dd62ae1742dfb33f4967d781a3
gkichan <gporodko@gmail.com>
2018-04-25 14:41:34 +0300

    before pull MakKos

--- app/js/init.js
+++ app/js/init.js
@@ -8,12 +8,12 @@ $('input, select').styler();
 $('#upload-file-styler').styler('destroy');
 
 //Slick Slider Init
-$('.hot-products-carousel').slick({
-    arrows: false,
-    dots: true,
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000
-});
+// $('.hot-products-carousel').slick({
+//     arrows: false,
+//     dots: true,
+//     infinite: true,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 3000
+// });
